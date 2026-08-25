743399d7777a004d2c836008d3ad39c94909988e
vbhavanisys <13472bca2024@princescience.in>
2026-08-25 20:49:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/StudySync/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
